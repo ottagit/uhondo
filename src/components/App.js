e420d1dff4241c0ba6cd4083e3ab0b4f79c692e4
ottagit <ottacc66@gmail.com>
2018-12-23 00:41:31 +0300

    Save user in App component state
    By so doing the user email can be passed
    down to 'container' components any time
    there's need to acess the email these
    containers

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 //import { hot } from 'react-hot-loader/root';
 import './App.css';
 import LoginContainer from './LoginContainer';
 
 class App extends Component {
+  state = { user: null };
+  
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({ user });
+      }
+    });
+  }
 
   render() {
     return (
