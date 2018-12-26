4f8ba1f9b139a84b7556f548db1846217aa5c737
ottagit <ottacc66@gmail.com>
2018-12-26 19:15:54 +0300

    build skeleton ui for chat container

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -10,10 +10,22 @@ class ChatContainer extends Component {
 
   render() {
     return (
-      <div id="ChatContainer">
+      <div id="ChatContainer" className="inner-container">
         <Header> 
           <button className="red" onClick={this.handleLogout} >Logout</button>
         </Header>
+
+        <div id="message-container">
+        </div>
+
+        <div id="chat-input">
+          <textarea placeholder="Your message goes here..." />
+          <button>
+            <svg viewBox="0 0 24 24">
+              <path fill="#424242" d="M2,21L23,12L2,3V10L17,12,L2,14V21Z" />
+            </svg>
+          </button>
+        </div>
       </div>
     );
   }
--- src/sections/Header.js
+++ src/sections/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = (props) => {
   return (
     <div id="Header">
-      <img src="../../assets/logo.png" alt="Logo" />
+      <img src="/assets/logo.png" alt="Logo" />
       <h1>Uhondo</h1>
       {props.children}
     </div>
