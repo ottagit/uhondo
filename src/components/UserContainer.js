52ffd140afe9139328d8ada97cee3e17b6cff1ef
ottagit <ottacc66@gmail.com>
2019-01-02 15:40:42 +0300

    create basic user profile page

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -3,15 +3,42 @@ import { Link } from 'react-router-dom';
 import Header from '../sections/Header';
 
 class UserContainer extends Component {
+  renderedUserEmail = false;
+
+  getAuthor = author => {
+    if (!this.renderedUserEmail) {
+      this.renderedUserEmail = true;
+      return <p className="author">{author}</p>
+    }
+  };
+
   render() {
     return (
-      <div id="UserContainer">
+      <div id="UserContainer" className="inner-container" >
         <Header>
           <Link to="/">
             <button className="red">Back to chat</button>
           </Link>
         </Header>
-        <h1>Hello from User {this.props.match.params.id}</h1>
+        { this.props.messagesLoaded ? (
+          <div id="message-container">
+            { this.props.messages.map(msg => {
+              if (msg.user_id === this.props.userID) {
+                return (
+                  <div key={msg.id} className="message">
+                    { this.getAuthor(msg.author) }
+                    <p>{msg.msg}</p>
+                  </div>
+                );
+              }})
+            }
+          </div>
+        ) : (
+          <div id="loading-container">
+            <img src="/assets/logo.svg" alt="Logo" id="loader" />
+          </div>
+        )
+        }
       </div>
     );
   }
